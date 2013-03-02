61b0e3ec8314e73a001d5b595ed41807eb81a439
Shawn Wildermuth <shawn@wildermuth.com>
2013-03-02 17:45:27 -0500

    Initial Drop

---
+++ PPGApp/www/js/app.js
@@ -0,0 +1,42 @@
+﻿// app.js
+(function (app, $) {
+ 
+  if (window.cordova) {
+    var url = "http://localhost/api/1/find/recent";
+  } else {
+    var url = "http://localhost:8888/recent.js";
+  }
+
+  app.vm = {
+    items: ko.observableArray([]),
+    msg: ko.observable(""),
+    getRecent: function () {
+      app.vm.msg("Loading");
+      app.vm.items.removeAll();
+      $.getJSON(url)
+      .success(function (data) {
+        $.each(data.MediaResults, function (i, item) {
+          app.vm.items.push(item)
+        });
+        app.vm.msg("found: " + app.vm.items().length);
+      })
+      .fail(function (error) {
+        app.vm.msg("failed!");
+      });
+
+    }
+  };
+
+  function init() {
+    ko.applyBindings(app.vm);
+    app.vm.getRecent();
+  }
+
+  // Startup (so you can test outside the browser)
+  if (window.cordova) {
+    document.addEventListener("deviceready", init, false);
+  } else {
+    $(document).ready(init);
+  }
+
+})(window.app = window.app || {}, $);
---
+++ PPGApp/www/js/settings.js
@@ -0,0 +1,13 @@
+﻿// settings.js
+(function (settings, $) {
+ 
+  settings.vm = {
+    showOnlyMovies: ko.observable(false),
+    showLatestOnStartup: ko.observable(true)
+  };
+
+  $(document).ready(function () {
+    ko.applyBindings(settings.vm);
+  });
+
+})(window.settings = window.settings || {}, $);
