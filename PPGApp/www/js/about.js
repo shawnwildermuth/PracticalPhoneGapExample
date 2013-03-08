79371516913139961cf65073320e3dad01d5d919
Shawn Wildermuth <shawn@wildermuth.com>
2013-03-08 17:04:39 -0500

    Updated with zepto instead of jQuery

--- PPGApp/www/js/about.js
+++ PPGApp/www/js/about.js
@@ -2,28 +2,8 @@
 (function (about, $) {
  
 
-  about.vm = {
-    geoInfo: ko.observable(""),
-    getGeoInfo: function () {
-      about.vm.geoInfo("Getting Geolocation Info");
-      navigator.geolocation.getCurrentPosition(
-        // Success
-        function (data) {
-          var loc = data.coords;
-          about.vm.geoInfo(loc.latitude + " x " + loc.longitude + " (Alt: " + loc.altitude + ")");
-        },
-        // Error
-        function (err) {
-          about.vm.geoInfo("Failed to get position");
-        },
-        // Options
-        { enableHighAccuracy: true });
-    }
-  };
 
   about.init = function () {
-    ko.applyBindings(about.vm);
-    about.vm.getGeoInfo();
   };
 
 })(window.about = window.about || {}, $);
--- PPGApp/www/js/index.js
+++ PPGApp/www/js/index.js
@@ -1,35 +1,49 @@
 ﻿// init.js
 (function (index, $) {
- 
+
   if (window.cordova) {
     var url = "http://howtowat.ch/api/1/find/recent";
   } else {
-    var url = "http://localhost:8888/recent.js";
+    var url = "http://localhost:8888/recent.json";
   }
 
-  app.vm = {
+  index.vm = {
     items: ko.observableArray([]),
     msg: ko.observable(""),
     getRecent: function () {
-      app.vm.msg("Loading");
-      app.vm.items.removeAll();
-      $.getJSON(url)
-      .success(function (data) {
-        $.each(data.MediaResults, function (i, item) {
-          app.vm.items.push(item)
-        });
-        app.vm.msg("found: " + app.vm.items().length);
-      })
-      .fail(function (error) {
-        app.vm.msg("failed: " + error);
+      index.vm.msg("Loading");
+      index.vm.items.removeAll();
+      $.ajax({
+        url: url,
+        dataType: 'json',
+        success: function (data) {
+          $.each(data.MediaResults, function (i, item) {
+            index.vm.items.push(item)
+          });
+          index.vm.msg("Found " + index.vm.items().length + " Result(s)");
+        },
+        error: function (xhr, type) {
+          index.vm.msg("Failed to load data");
+        }
       });
 
     }
   };
 
   index.init = function () {
-    ko.applyBindings(app.vm);
-    app.vm.getRecent();
+    ko.applyBindings(index.vm);
+    var canGetRecent = true;
+    if (window.cordova && navigator.connection)
+    {
+      // Check connection first
+      if (navigator.connection.type && navigator.connection.type === Connection.NONE)
+      {
+        // Cancel get recent and set the text that there isn't a connection
+        canGetRecent = false;
+        vm.index.msg("No Connection Available");
+      }
+    }
+    if (canGetRecent) index.vm.getRecent();
   };
 
 })(window.index = window.index || {}, $);
